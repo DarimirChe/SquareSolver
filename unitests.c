666cb1e6156ca5a4bc644fe6e4720ec43e70f5da
DarimirChe <Darimir.Chernenko@yandex.ru>
2026-08-21 13:42:47 +0300

    change unitests

--- unitests.c
+++ unitests.c
@@ -7,19 +7,26 @@ void testSolveLinear(double b, double c, enum countSolution correctReturn, doubl
 void printIncorrectReturn(double b, double c, enum countSolution currentReturn, enum countSolution correctReturn);
 void printIncorrectX(double b, double c, double currentX, double correctX);
 
-int testNumber = 1;
-
 int main() {
-    testSolveLinear(1, 1, ONE_SOLUTION, -1);
-    testSolveLinear(1, -2, ONE_SOLUTION, 2);
-    testSolveLinear(2, 3, ONE_SOLUTION, -1.5);
-    testSolveLinear(-2, -1, ONE_SOLUTION, -0.5);
-    testSolveLinear(-1.5, 3, ONE_SOLUTION, 2);
-    testSolveLinear(0.5, -1.25, ONE_SOLUTION, 2.5);
-    testSolveLinear(-3.5, 0, ONE_SOLUTION, 0);
+    printf("Test 1 processing\n");
+    testSolveLinear(1,    1,     ONE_SOLUTION, -1);
+    printf("Test 2 processing\n");
+    testSolveLinear(1,    -2,    ONE_SOLUTION, 2);
+    printf("Test 3 processing\n");
+    testSolveLinear(2,    3,     ONE_SOLUTION, -1.5);
+    printf("Test 4 processing\n");
+    testSolveLinear(-2,   -1,    ONE_SOLUTION, -0.5);
+    printf("Test 5 processing\n");
+    testSolveLinear(-1.5, 3,     ONE_SOLUTION, 2);
+    printf("Test 6 processing\n");
+    testSolveLinear(0.5,  -1.25, ONE_SOLUTION, 2.5);
+    printf("Test 7 processing\n");
+    testSolveLinear(-3.5, 0,     ONE_SOLUTION, 10);
 
-    testSolveLinear(0, 0, INFINITY_SOLUTIONS, 0);
+    printf("Test 8 processing\n");
+    testSolveLinear(0, 0, ONE_SOLUTION, 0);
 
+    printf("Test 9 processing\n");
     testSolveLinear(0, 1, NO_SOLUTION, 0);
     return 0;
 }
@@ -35,21 +42,20 @@ void testSolveLinear(double b, double c, enum countSolution correctReturn, doubl
     } else if (currentX != correctX) {
         printIncorrectX(b, c, currentX, correctX);
     }
-    testNumber++;
 }
 
 /* Prints the test summary if the return is incorrect */
 void printIncorrectReturn(double b, double c, enum countSolution currentReturn, enum countSolution correctReturn) {
-    printf("Test #%d failed: incorrect return.\n", testNumber);
+    printf("Test failed: incorrect return.\n");
     printf("Input: b = %lg, c = %lg\n", b, c);
     printf("Current return: %d\n", currentReturn);
-    printf("Correct return: %d\n\n", correctReturn);
+    printf("Correct return: %d\n", correctReturn);
 }
 
 /* Prints the test summary if the x is incorrect */
 void printIncorrectX(double b, double c, double currentX, double correctX) {
-    printf("Test #%d failed: incorrect x.\n", testNumber);
+    printf("Test failed: incorrect x.\n");
     printf("Input: b = %lg, c = %lg\n", b, c);
     printf("Current x: %lg\n", currentX);
-    printf("Correct x: %lg\n\n", correctX);
+    printf("Correct x: %lg\n", correctX);
 }
