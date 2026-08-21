526aa5a9cc354541aff7b943b280cc62fd3e5860
DarimirChe <Darimir.Chernenko@yandex.ru>
2026-08-21 14:16:52 +0300

    implemented the tests through a structure and a loop

--- unitests.c
+++ unitests.c
@@ -2,59 +2,70 @@
 
 #include "solvers.h"
 
-void testSolveLinear(double b, double c, enum countSolution correctReturn, double correctX);
+bool testSolveLinear(double b, double c, enum countSolution correctReturn, double correctX);
 
 void printIncorrectReturn(double b, double c, enum countSolution currentReturn, enum countSolution correctReturn);
 void printIncorrectX(double b, double c, double currentX, double correctX);
 
+struct dataTest {
+    double b;
+    double c;
+    enum countSolution currentCountSolution;
+    double currentX;
+};
+
+const int COUNT_TESTS = 9;
+
 int main() {
-    printf("Test 1 processing\n");
-    testSolveLinear(1,    1,     ONE_SOLUTION, -1);
-    printf("Test 2 processing\n");
-    testSolveLinear(1,    -2,    ONE_SOLUTION, 2);
-    printf("Test 3 processing\n");
-    testSolveLinear(2,    3,     ONE_SOLUTION, -1.5);
-    printf("Test 4 processing\n");
-    testSolveLinear(-2,   -1,    ONE_SOLUTION, -0.5);
-    printf("Test 5 processing\n");
-    testSolveLinear(-1.5, 3,     ONE_SOLUTION, 2);
-    printf("Test 6 processing\n");
-    testSolveLinear(0.5,  -1.25, ONE_SOLUTION, 2.5);
-    printf("Test 7 processing\n");
-    testSolveLinear(-3.5, 0,     ONE_SOLUTION, 10);
+    struct dataTest tests[COUNT_TESTS] = {
+        {.b = 1, .c = 1, .currentCountSolution = ONE_SOLUTION, .currentX = -1},
+        {.b = 1, .c = -2, .currentCountSolution = ONE_SOLUTION, .currentX = 2},
+        {.b = 2, .c = 3, .currentCountSolution = ONE_SOLUTION, .currentX = -1.5},
+        {.b = -2, .c = -1, .currentCountSolution = ONE_SOLUTION, .currentX = -0.5},
+        {.b = -1.5, .c = 3, .currentCountSolution = ONE_SOLUTION, .currentX = 2},
+        {.b = 0.5, .c = -1.25, .currentCountSolution = ONE_SOLUTION, .currentX = 2.5},
+        {.b = -3.5, .c = 0, .currentCountSolution = ONE_SOLUTION, .currentX = 2.5},
+        {.b = 0, .c = 0, .currentCountSolution = INFINITY_SOLUTIONS, .currentX = 0},
+        {.b = 0, .c = 1, .currentCountSolution = NO_SOLUTION, .currentX = 0}
+    };
 
-    printf("Test 8 processing\n");
-    testSolveLinear(0, 0, ONE_SOLUTION, 0);
-
-    printf("Test 9 processing\n");
-    testSolveLinear(0, 1, NO_SOLUTION, 0);
+    for (int i = 0; i < COUNT_TESTS; i++) {
+        printf("Test %d processing...\n", i + 1);
+        if (testSolveLinear(tests[i].b, tests[i].c, tests[i].currentCountSolution, tests[i].currentX)) {
+            printf("Test %d passed.\n", i + 1);
+        }
+        printf("\n");
+    }
     return 0;
 }
 
 /* Tests the solveLinear() function, accepts coefficients b, c of the equation of the form: bx + c = 0, 
 the correct return and x for these coefficients */
-void testSolveLinear(double b, double c, enum countSolution correctReturn, double correctX) {
+bool testSolveLinear(double b, double c, enum countSolution correctCountSolution, double correctX) {
     double currentX = 0;
-    enum countSolution currerntReturn = solveLinear(b, c, &currentX);
+    enum countSolution currentCountSolution = solveLinear(b, c, &currentX);
 
-    if (currerntReturn != correctReturn) {
-        printIncorrectReturn(b, c, currerntReturn, correctReturn);
+    if (currentCountSolution != correctCountSolution) {
+        printIncorrectReturn(b, c, currentCountSolution, correctCountSolution);
+        return false;
     } else if (currentX != correctX) {
         printIncorrectX(b, c, currentX, correctX);
+        return false;
     }
+    return true;
 }
 
 /* Prints the test summary if the return is incorrect */
-void printIncorrectReturn(double b, double c, enum countSolution currentReturn, enum countSolution correctReturn) {
-    printf("Test failed: incorrect return.\n");
+void printIncorrectReturn(double b, double c, enum countSolution currentCountSolution, enum countSolution correctCountSolution) {
+    printf("Incorrect return.\n");
     printf("Input: b = %lg, c = %lg\n", b, c);
-    printf("Current return: %d\n", currentReturn);
-    printf("Correct return: %d\n", correctReturn);
+    printf("Current return: %d\n", currentCountSolution);
+    printf("Correct return: %d\n", correctCountSolution);
 }
 
 /* Prints the test summary if the x is incorrect */
 void printIncorrectX(double b, double c, double currentX, double correctX) {
-    printf("Test failed: incorrect x.\n");
+    printf("Incorrect x.\n");
     printf("Input: b = %lg, c = %lg\n", b, c);
     printf("Current x: %lg\n", currentX);
     printf("Correct x: %lg\n", correctX);
